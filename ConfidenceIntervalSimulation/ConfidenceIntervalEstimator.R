0bbc46a968e2508108416a6c9d620c6af54c665b
EJay.Guo <ejay.guo@gmail.com>
2021-05-14 20:41:37 -0600

    update CI final results and code

--- ConfidenceIntervalSimulation/ConfidenceIntervalEstimator.R
+++ ConfidenceIntervalSimulation/ConfidenceIntervalEstimator.R
@@ -20,12 +20,392 @@ source('./TTML/FeatureConverter.R')
 # df_full = Full_Simulation(10000, 500, 10, 100)
 # Result02
 # df_full = Full_Simulation(10000, 500, 10, 500)
+# Result03
+# df_full = Full_Simulation(25000, 1000, 10, 100)
 
-Full_Simulation<- function(num_train, num_test, d, num_bootstrap){
+# vec_train_size5 = seq(1000, 5000, by=1000)
+# vec_test_size5 = seq(50, 250, by=50)
+# vec_trials5 = seq(1000, 5000, by=1000)
+# list_rtn_5 = Bias_Full_Simulation(5, vec_train_size5, vec_test_size5, 10)
+
+# vec_train_size10 = seq(1000, 10000, by=1000)
+# vec_test_size10 = seq(50, 500, by=50)
+# vec_trials10 = seq(1000, 10000, by=1000)
+# list_rtn_10 = Bias_Full_Simulation(10, vec_train_size10, vec_test_size10, 10)
+
+# vec_train_size15 = seq(1000, 15000, by=1000)
+# vec_test_size15 = seq(50, 750, by=50)
+# vec_trials15 = seq(1000, 15000, by=1000)
+# list_rtn_15 = Bias_Full_Simulation(15, vec_train_size15, vec_test_size15, 10)
+
+# vec_train_size20 = seq(1000, 20000, by=1000)
+# vec_test_size20 = seq(50, 1000, by=50)
+# vec_trials20 = seq(1000, 20000, by=1000)
+# list_rtn_20 = Bias_Full_Simulation(20, vec_train_size20, vec_test_size20, 10)
+
+Bias_Full_Simulation<- function(num_trials, vec_train_size, vec_test_size, d){
+  
+  vec_rmse_sl_rf = c()
+  vec_rmse_sl_bart = c()
+  vec_rmse_tl_rf = c()
+  vec_rmse_tl_bart = c()
+  vec_rmse_xl_rf = c()
+  vec_rmse_xl_bart = c()
+  
+  vec_bias_sl_rf = c()
+  vec_bias_sl_bart = c()
+  vec_bias_tl_rf = c()
+  vec_bias_tl_bart = c()
+  vec_bias_xl_rf = c()
+  vec_bias_xl_bart = c()
+  
+  vec_rmse_sl_rf_B = c()
+  vec_rmse_sl_bart_B = c()
+  vec_rmse_tl_rf_B = c()
+  vec_rmse_tl_bart_B = c()
+  vec_rmse_xl_rf_B = c()
+  vec_rmse_xl_bart_B = c()
+  
+  vec_bias_sl_rf_B = c()
+  vec_bias_sl_bart_B = c()
+  vec_bias_tl_rf_B = c()
+  vec_bias_tl_bart_B = c()
+  vec_bias_xl_rf_B = c()
+  vec_bias_xl_bart_B = c()
+  
+  vec_rmse_sl_rf_K = c()
+  vec_rmse_sl_bart_K = c()
+  vec_rmse_tl_rf_K = c()
+  vec_rmse_tl_bart_K = c()
+  vec_rmse_xl_rf_K = c()
+  vec_rmse_xl_bart_K = c()
+  
+  vec_bias_sl_rf_K = c()
+  vec_bias_sl_bart_K = c()
+  vec_bias_tl_rf_K = c()
+  vec_bias_tl_bart_K = c()
+  vec_bias_xl_rf_K = c()
+  vec_bias_xl_bart_K = c()
+  
+  for(i in 1:num_trials){
+    num_train = vec_train_size[i]
+    num_test = vec_test_size[i]
+    message(sprintf("==========Running %d of %d Trials, Train Size: %d, Test Size: %d==========", i, num_trials, num_train, num_test))
+
+    list_result_i = Bias_One_Simulation(num_train, num_test, d)
+    
+    rmse_sl_rf = list_result_i$rmse_sl_rf
+    rmse_sl_bart = list_result_i$rmse_sl_bart
+    rmse_tl_rf = list_result_i$rmse_tl_rf
+    rmse_tl_bart = list_result_i$rmse_tl_bart
+    rmse_xl_rf = list_result_i$rmse_xl_rf
+    rmse_xl_bart = list_result_i$rmse_xl_bart
+    vec_rmse_sl_rf = append(vec_rmse_sl_rf, rmse_sl_rf)
+    vec_rmse_sl_bart = append(vec_rmse_sl_bart, rmse_sl_bart)
+    vec_rmse_tl_rf = append(vec_rmse_tl_rf, rmse_tl_rf)
+    vec_rmse_tl_bart = append(vec_rmse_tl_bart, rmse_tl_bart)
+    vec_rmse_xl_rf = append(vec_rmse_xl_rf, rmse_xl_rf)
+    vec_rmse_xl_bart = append(vec_rmse_xl_bart, rmse_xl_bart)
+    
+    bias_sl_rf = list_result_i$bias_sl_rf
+    bias_sl_bart = list_result_i$bias_sl_bart
+    bias_tl_rf = list_result_i$bias_tl_rf
+    bias_tl_bart = list_result_i$bias_tl_bart
+    bias_xl_rf = list_result_i$bias_xl_rf
+    bias_xl_bart = list_result_i$bias_xl_bart
+    vec_bias_sl_rf = append(vec_bias_sl_rf, bias_sl_rf)
+    vec_bias_sl_bart = append(vec_bias_sl_bart, bias_sl_bart)
+    vec_bias_tl_rf = append(vec_bias_tl_rf, bias_tl_rf)
+    vec_bias_tl_bart = append(vec_bias_tl_bart, bias_tl_bart)
+    vec_bias_xl_rf = append(vec_bias_xl_rf, bias_xl_rf)
+    vec_bias_xl_bart = append(vec_bias_xl_bart, bias_xl_bart)
+    
+    rmse_sl_rf_B = list_result_i$rmse_sl_rf_B
+    rmse_sl_bart_B = list_result_i$rmse_sl_bart_B
+    rmse_tl_rf_B = list_result_i$rmse_tl_rf_B
+    rmse_tl_bart_B = list_result_i$rmse_tl_bart_B
+    rmse_xl_rf_B = list_result_i$rmse_xl_rf_B
+    rmse_xl_bart_B = list_result_i$rmse_xl_bart_B
+    vec_rmse_sl_rf_B = append(vec_rmse_sl_rf_B, rmse_sl_rf_B)
+    vec_rmse_sl_bart_B = append(vec_rmse_sl_bart_B, rmse_sl_bart_B)
+    vec_rmse_tl_rf_B = append(vec_rmse_tl_rf_B, rmse_tl_rf_B)
+    vec_rmse_tl_bart_B = append(vec_rmse_tl_bart_B, rmse_tl_bart_B)
+    vec_rmse_xl_rf_B = append(vec_rmse_xl_rf_B, rmse_xl_rf_B)
+    vec_rmse_xl_bart_B = append(vec_rmse_xl_bart_B, rmse_xl_bart_B)
+    
+    bias_sl_rf_B = list_result_i$bias_sl_rf_B
+    bias_sl_bart_B = list_result_i$bias_sl_bart_B
+    bias_tl_rf_B = list_result_i$bias_tl_rf_B
+    bias_tl_bart_B = list_result_i$bias_tl_bart_B
+    bias_xl_rf_B = list_result_i$bias_xl_rf_B
+    bias_xl_bart_B = list_result_i$bias_xl_bart_B
+    vec_bias_sl_rf_B = append(vec_bias_sl_rf_B, bias_sl_rf_B)
+    vec_bias_sl_bart_B = append(vec_bias_sl_bart_B, bias_sl_bart_B)
+    vec_bias_tl_rf_B = append(vec_bias_tl_rf_B, bias_tl_rf_B)
+    vec_bias_tl_bart_B = append(vec_bias_tl_bart_B, bias_tl_bart_B)
+    vec_bias_xl_rf_B = append(vec_bias_xl_rf_B, bias_xl_rf_B)
+    vec_bias_xl_bart_B = append(vec_bias_xl_bart_B, bias_xl_bart_B)
+    
+    rmse_sl_rf_K = list_result_i$rmse_sl_rf_K
+    rmse_sl_bart_K = list_result_i$rmse_sl_bart_K
+    rmse_tl_rf_K = list_result_i$rmse_tl_rf_K
+    rmse_tl_bart_K = list_result_i$rmse_tl_bart_K
+    rmse_xl_rf_K = list_result_i$rmse_xl_rf_K
+    rmse_xl_bart_K = list_result_i$rmse_xl_bart_K
+    vec_rmse_sl_rf_K = append(vec_rmse_sl_rf_K, rmse_sl_rf_K)
+    vec_rmse_sl_bart_K = append(vec_rmse_sl_bart_K, rmse_sl_bart_K)
+    vec_rmse_tl_rf_K = append(vec_rmse_tl_rf_K, rmse_tl_rf_K)
+    vec_rmse_tl_bart_K = append(vec_rmse_tl_bart_K, rmse_tl_bart_K)
+    vec_rmse_xl_rf_K = append(vec_rmse_xl_rf_K, rmse_xl_rf_K)
+    vec_rmse_xl_bart_K = append(vec_rmse_xl_bart_K, rmse_xl_bart_K)
+    
+    bias_sl_rf_K = list_result_i$bias_sl_rf_K
+    bias_sl_bart_K = list_result_i$bias_sl_bart_K
+    bias_tl_rf_K = list_result_i$bias_tl_rf_K
+    bias_tl_bart_K = list_result_i$bias_tl_bart_K
+    bias_xl_rf_K = list_result_i$bias_xl_rf_K
+    bias_xl_bart_K = list_result_i$bias_xl_bart_K
+    vec_bias_sl_rf_K = append(vec_bias_sl_rf_K, bias_sl_rf_K)
+    vec_bias_sl_bart_K = append(vec_bias_sl_bart_K, bias_sl_bart_K)
+    vec_bias_tl_rf_K = append(vec_bias_tl_rf_K, bias_tl_rf_K)
+    vec_bias_tl_bart_K = append(vec_bias_tl_bart_K, bias_tl_bart_K)
+    vec_bias_xl_rf_K = append(vec_bias_xl_rf_K, bias_xl_rf_K)
+    vec_bias_xl_bart_K = append(vec_bias_xl_bart_K, bias_xl_bart_K)
+  }
+  
+  DF_RMSE <- data.frame(
+    sl_rf = vec_rmse_sl_rf, 
+    sl_bart = vec_rmse_sl_bart, 
+    tl_rf = vec_rmse_tl_rf,
+    tl_bart = vec_rmse_tl_bart,
+    xl_rf = vec_rmse_xl_rf,
+    xl_bart = vec_rmse_xl_bart,
+    
+    sl_rf_B = vec_rmse_sl_rf_B, 
+    sl_bart_B = vec_rmse_sl_bart_B, 
+    tl_rf_B = vec_rmse_tl_rf_B,
+    tl_bart_B = vec_rmse_tl_bart_B,
+    xl_rf_B = vec_rmse_xl_rf_B,
+    xl_bart_B = vec_rmse_xl_bart_B,
+    
+    sl_rf_K = vec_rmse_sl_rf_K, 
+    sl_bart_K = vec_rmse_sl_bart_K, 
+    tl_rf_K = vec_rmse_tl_rf_K,
+    tl_bart_K = vec_rmse_tl_bart_K,
+    xl_rf_K = vec_rmse_xl_rf_K,
+    xl_bart_K = vec_rmse_xl_bart_K
+  )
+  
+  DF_RMSE <- data.frame(
+    sl_rf = vec_rmse_sl_rf, 
+    sl_bart = vec_rmse_sl_bart, 
+    tl_rf = vec_rmse_tl_rf,
+    tl_bart = vec_rmse_tl_bart,
+    xl_rf = vec_rmse_xl_rf,
+    xl_bart = vec_rmse_xl_bart,
+    
+    sl_rf_B = vec_rmse_sl_rf_B, 
+    sl_bart_B = vec_rmse_sl_bart_B, 
+    tl_rf_B = vec_rmse_tl_rf_B,
+    tl_bart_B = vec_rmse_tl_bart_B,
+    xl_rf_B = vec_rmse_xl_rf_B,
+    xl_bart_B = vec_rmse_xl_bart_B,
+    
+    sl_rf_K = vec_rmse_sl_rf_K, 
+    sl_bart_K = vec_rmse_sl_bart_K, 
+    tl_rf_K = vec_rmse_tl_rf_K,
+    tl_bart_K = vec_rmse_tl_bart_K,
+    xl_rf_K = vec_rmse_xl_rf_K,
+    xl_bart_K = vec_rmse_xl_bart_K
+  )
+  
+  DF_BIAS <- data.frame(
+    sl_rf = vec_bias_sl_rf, 
+    sl_bart = vec_bias_sl_bart, 
+    tl_rf = vec_bias_tl_rf,
+    tl_bart = vec_bias_tl_bart,
+    xl_rf = vec_bias_xl_rf,
+    xl_bart = vec_bias_xl_bart,
+    
+    sl_rf_B = vec_bias_sl_rf_B, 
+    sl_bart_B = vec_bias_sl_bart_B, 
+    tl_rf_B = vec_bias_tl_rf_B,
+    tl_bart_B = vec_bias_tl_bart_B,
+    xl_rf_B = vec_bias_xl_rf_B,
+    xl_bart_B = vec_bias_xl_bart_B,
+    
+    sl_rf_K = vec_bias_sl_rf_K, 
+    sl_bart_K = vec_bias_sl_bart_K, 
+    tl_rf_K = vec_bias_tl_rf_K,
+    tl_bart_K = vec_bias_tl_bart_K,
+    xl_rf_K = vec_bias_xl_rf_K,
+    xl_bart_K = vec_bias_xl_bart_K
+  )
+  
+  list_rtn = list(rmse = DF_RMSE, bias = DF_BIAS)
+  
+  return(list_rtn)
+}
+
+Bias_One_Simulation<- function(num_train, num_test, d){
+  simulated_experiment <- simulate_causal_experiment(
+    ntrain = num_train,
+    ntest = num_test,
+    dim = d)
+  
+  feature_train_raw <- simulated_experiment$feat_tr
+  feature_test_raw <- simulated_experiment$feat_te
+  w_train <- simulated_experiment$W_tr
+  yobs_train <- simulated_experiment$Yobs_tr
+  cate_true <- simulated_experiment$tau_te
+  
+  feature_train = feature_train_raw
+  feature_test = feature_test_raw
+  
+  list_rtn_B = Convert_Dataset_Binary(feature_train_raw, feature_test_raw, depth_bt = 8, type_root="mean")
+  feature_train_B = list_rtn_B$train
+  feature_test_B = list_rtn_B$test
+  
+  list_rtn_K = Convert_Dataset_KBin(feature_train_raw, feature_test_raw, k_bin=16)
+  feature_train_K = list_rtn_K$train
+  feature_test_K = list_rtn_K$test
+  
+  # print(head(feature_train))
+  # print(head(feature_train_B))
+  # print(head(feature_train_K))
+  
+  message(sprintf("Learning Over Raw Data..."))
+  sl_rf <- S_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
+  sl_bart <- S_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
+  
+  tl_rf <- T_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
+  tl_bart <- T_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
+  
+  xl_rf <- X_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
+  xl_bart <- X_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
+  
+  cate_esti_sl_rf <- EstimateCate(sl_rf, feature_test)
+  cate_esti_sl_bart <- EstimateCate(sl_bart, feature_test)
+  cate_esti_tl_rf <- EstimateCate(tl_rf, feature_test)
+  cate_esti_tl_bart <- EstimateCate(tl_bart, feature_test)
+  cate_esti_xl_rf <- EstimateCate(xl_rf, feature_test)
+  cate_esti_xl_bart <- EstimateCate(xl_bart, feature_test)
+  
+  # cate_true <- cate_esti_tl_rf
+  cate_true <- simulated_experiment$tau_te
+  
+  bias_sl_rf = abs(mean(cate_esti_sl_rf - cate_true))
+  bias_sl_bart = abs(mean(cate_esti_sl_bart - cate_true))
+  bias_tl_rf = abs(mean(cate_esti_tl_rf - cate_true))
+  bias_tl_bart = abs(mean(cate_esti_tl_bart - cate_true))
+  bias_xl_rf = abs(mean(cate_esti_xl_rf - cate_true))
+  bias_xl_bart = abs(mean(cate_esti_xl_bart - cate_true))
+  
+  rmse_sl_rf = mean((cate_esti_sl_rf - cate_true) ^ 2)
+  rmse_sl_bart = mean((cate_esti_sl_bart - cate_true) ^ 2)
+  rmse_tl_rf = mean((cate_esti_tl_rf - cate_true) ^ 2)
+  rmse_tl_bart = mean((cate_esti_tl_bart - cate_true) ^ 2)
+  rmse_xl_rf = mean((cate_esti_xl_rf - cate_true) ^ 2)
+  rmse_xl_bart = mean((cate_esti_xl_bart - cate_true) ^ 2)
+  
+  message(sprintf("Learning Over Binary Data..."))
+  sl_rf_B <- S_RF(feat = feature_train_B, tr = w_train, yobs = yobs_train)
+  sl_bart_B <- S_BART(feat = feature_train_B, tr = w_train, yobs = yobs_train)
+  
+  tl_rf_B <- T_RF(feat = feature_train_B, tr = w_train, yobs = yobs_train)
+  tl_bart_B <- T_BART(feat = feature_train_B, tr = w_train, yobs = yobs_train)
+  
+  xl_rf_B <- X_RF(feat = feature_train_B, tr = w_train, yobs = yobs_train)
+  xl_bart_B <- X_BART(feat = feature_train_B, tr = w_train, yobs = yobs_train)
+  
+  cate_esti_sl_rf_B <- EstimateCate(sl_rf_B, feature_test_B)
+  cate_esti_sl_bart_B <- EstimateCate(sl_bart_B, feature_test_B)
+  cate_esti_tl_rf_B <- EstimateCate(tl_rf_B, feature_test_B)
+  cate_esti_tl_bart_B <- EstimateCate(tl_bart_B, feature_test_B)
+  cate_esti_xl_rf_B <- EstimateCate(xl_rf_B, feature_test_B)
+  cate_esti_xl_bart_B <- EstimateCate(xl_bart_B, feature_test_B)
+  
+  # cate_true_B <- cate_esti_tl_rf_B
+  cate_true_B <- simulated_experiment$tau_te
+  
+  bias_sl_rf_B = abs(mean(cate_esti_sl_rf_B - cate_true_B))
+  bias_sl_bart_B = abs(mean(cate_esti_sl_bart_B - cate_true_B))
+  bias_tl_rf_B = abs(mean(cate_esti_tl_rf_B - cate_true_B))
+  bias_tl_bart_B = abs(mean(cate_esti_tl_bart_B - cate_true_B))
+  bias_xl_rf_B = abs(mean(cate_esti_xl_rf_B - cate_true_B))
+  bias_xl_bart_B = abs(mean(cate_esti_xl_bart_B - cate_true_B))
+  
+  rmse_sl_rf_B = mean((cate_esti_sl_rf_B - cate_true_B) ^ 2)
+  rmse_sl_bart_B = mean((cate_esti_sl_bart_B - cate_true_B) ^ 2)
+  rmse_tl_rf_B = mean((cate_esti_tl_rf_B - cate_true_B) ^ 2)
+  rmse_tl_bart_B = mean((cate_esti_tl_bart_B - cate_true_B) ^ 2)
+  rmse_xl_rf_B = mean((cate_esti_xl_rf_B - cate_true_B) ^ 2)
+  rmse_xl_bart_B = mean((cate_esti_xl_bart_B - cate_true_B) ^ 2)
+  
+  message(sprintf("Learning Over Discrete Data..."))
+  sl_rf_K <- S_RF(feat = feature_train_K, tr = w_train, yobs = yobs_train)
+  sl_bart_K <- S_BART(feat = feature_train_K, tr = w_train, yobs = yobs_train)
+  
+  tl_rf_K <- T_RF(feat = feature_train_K, tr = w_train, yobs = yobs_train)
+  tl_bart_K <- T_BART(feat = feature_train_K, tr = w_train, yobs = yobs_train)
+  
+  xl_rf_K <- X_RF(feat = feature_train_K, tr = w_train, yobs = yobs_train)
+  xl_bart_K <- X_BART(feat = feature_train_K, tr = w_train, yobs = yobs_train)
+  
+  
+  
+  cate_esti_sl_rf_K <- EstimateCate(sl_rf_K, feature_test_K)
+  cate_esti_sl_bart_K <- EstimateCate(sl_bart_K, feature_test_K)
+  cate_esti_tl_rf_K <- EstimateCate(tl_rf_K, feature_test_K)
+  cate_esti_tl_bart_K <- EstimateCate(tl_bart_K, feature_test_K)
+  cate_esti_xl_rf_K <- EstimateCate(xl_rf_K, feature_test_K)
+  cate_esti_xl_bart_K <- EstimateCate(xl_bart_K, feature_test_K)
+  
+  # cate_true_K <- cate_esti_tl_rf_K
+  cate_true_K <- simulated_experiment$tau_te
+  
+  bias_sl_rf_K = abs(mean(cate_esti_sl_rf_K - cate_true_K))
+  bias_sl_bart_K = abs(mean(cate_esti_sl_bart_K - cate_true_K))
+  bias_tl_rf_K = abs(mean(cate_esti_tl_rf_K - cate_true_K))
+  bias_tl_bart_K = abs(mean(cate_esti_tl_bart_K - cate_true_K))
+  bias_xl_rf_K = abs(mean(cate_esti_xl_rf_K - cate_true_K))
+  bias_xl_bart_K = abs(mean(cate_esti_xl_bart_K - cate_true_K))
+  
+  rmse_sl_rf_K = mean((cate_esti_sl_rf_K - cate_true_K) ^ 2)
+  rmse_sl_bart_K = mean((cate_esti_sl_bart_K - cate_true_K) ^ 2)
+  rmse_tl_rf_K = mean((cate_esti_tl_rf_K - cate_true_K) ^ 2)
+  rmse_tl_bart_K = mean((cate_esti_tl_bart_K - cate_true_K) ^ 2)
+  rmse_xl_rf_K = mean((cate_esti_xl_rf_K - cate_true_K) ^ 2)
+  rmse_xl_bart_K = mean((cate_esti_xl_bart_K - cate_true_K) ^ 2)
+  
+  
+  list_rtn = list(rmse_sl_rf = rmse_sl_rf, rmse_sl_bart = rmse_sl_bart, 
+                  rmse_tl_rf = rmse_tl_rf, rmse_tl_bart = rmse_tl_bart, 
+                  rmse_xl_rf = rmse_xl_rf, rmse_xl_bart = rmse_xl_bart,
+                  rmse_sl_rf_B = rmse_sl_rf_B, rmse_sl_bart_B = rmse_sl_bart_B,
+                  rmse_tl_rf_B = rmse_tl_rf_B, rmse_tl_bart_B = rmse_tl_bart_B,
+                  rmse_xl_rf_B = rmse_xl_rf_B, rmse_xl_bart_B = rmse_xl_bart_B,
+                  rmse_sl_rf_K = rmse_sl_rf_K, rmse_sl_bart_K = rmse_sl_bart_K,
+                  rmse_tl_rf_K = rmse_tl_rf_K, rmse_tl_bart_K = rmse_tl_bart_K,
+                  rmse_xl_rf_K = rmse_xl_rf_K, rmse_xl_bart_K = rmse_xl_bart_K,
+                  bias_sl_rf = bias_sl_rf, bias_sl_bart = bias_sl_bart, 
+                  bias_tl_rf = bias_tl_rf, bias_tl_bart = bias_tl_bart, 
+                  bias_xl_rf = bias_xl_rf, bias_xl_bart = bias_xl_bart,
+                  bias_sl_rf_B = bias_sl_rf_B, bias_sl_bart_B = bias_sl_bart_B,
+                  bias_tl_rf_B = bias_tl_rf_B, bias_tl_bart_B = bias_tl_bart_B,
+                  bias_xl_rf_B = bias_xl_rf_B, bias_xl_bart_B = bias_xl_bart_B,
+                  bias_sl_rf_K = bias_sl_rf_K, bias_sl_bart_K = bias_sl_bart_K,
+                  bias_tl_rf_K = bias_tl_rf_K, bias_tl_bart_K = bias_tl_bart_K,
+                  bias_xl_rf_K = bias_xl_rf_K, bias_xl_bart_K = bias_xl_bart_K)
+  
+  return(list_rtn)
+}
+
+CI_Full_Simulation<- function(num_train, num_test, d, num_bootstrap){
   
   list_rtn_raw = Estimate_Learner_CI_Coverage(num_train, num_test, d, num_bootstrap, type_data="", NULL)
   list_rtn_K = Estimate_Learner_CI_Coverage(num_train, num_test, d, num_bootstrap, type_data="Discretization", parameter_data=list(k_bin = 8))
-  list_rtn_B = Estimate_Learner_CI_Coverage(num_train, num_test, d, num_bootstrap, type_data="Binary", parameter_data=list(depth_bt = 3, type_root="mean"))
+  list_rtn_B = Estimate_Learner_CI_Coverage(num_train, num_test, d, num_bootstrap, type_data="Binary", parameter_data=list(depth_bt = 3, type_root="median"))
   
   vec_ci_cov_full = c(list_rtn_raw$vec_ci_cov, list_rtn_K$vec_ci_cov, list_rtn_B$vec_ci_cov)
   vec_ci_len_full = c(list_rtn_raw$vec_ci_len, list_rtn_K$vec_ci_len, list_rtn_B$vec_ci_len)
@@ -54,7 +434,6 @@ Estimate_Learner_CI_Coverage <- function(num_train, num_test, d, num_bootstrap, 
     if(type_data == "Binary"){ mark_suffix = "B" }
     else if (type_data == "Discretization"){ mark_suffix = "K" }
   }
-  
   
   mark_sl_rf = paste("S_RF", mark_suffix, sep="_")
   message(sprintf("----------%s Running Bootstrap...----------", mark_sl_rf))
@@ -143,7 +522,7 @@ Generate_Simulation <- function(num_train, num_test, d, type_data, parameter_dat
     }
     else if (type_data == "Discretization"){
       message(sprintf("Converting Real To Discretization"))
-      list_rtn_K = Convert_Dataset_KBin(feature_train, feature_test, parameter_data$k_bin)
+      list_rtn_K = Convert_Dataset_KBin(feature_train_raw, feature_test_raw, parameter_data$k_bin)
       feature_train = list_rtn_K$train
       feature_test = list_rtn_K$test
     }
@@ -160,7 +539,9 @@ Generate_Simulation <- function(num_train, num_test, d, type_data, parameter_dat
   xl_rf <- X_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
   xl_bart <- X_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
   
-  list_rtn = list(truth = cate_true, feature_test = feature_test, sl_rf = sl_rf, sl_bart = sl_bart, tl_rf = tl_rf, tl_bart = tl_bart, xl_rf = xl_rf, xl_bart = xl_bart)
+  cate_esti_tl_rf <- EstimateCate(tl_rf, feature_test)
+  
+  list_rtn = list(truth = cate_esti_tl_rf, cate_true = cate_true, feature_test = feature_test, sl_rf = sl_rf, sl_bart = sl_bart, tl_rf = tl_rf, tl_bart = tl_bart, xl_rf = xl_rf, xl_bart = xl_bart)
   
   return(list_rtn)
 }
--- ConfidenceIntervalSimulation/ResultPloter.R
+++ ConfidenceIntervalSimulation/ResultPloter.R
@@ -1,4 +1,6 @@
 library(ggrepel)
+library(reshape2)
+library(scales)
 
 # A_X = c(0.1, 0.2, 0.3)
 # A_Y = c(1, 2, 3)
@@ -31,7 +33,160 @@ library(ggrepel)
 #   ShapeGroup = C_Shape
 # )
 
-Ploter<- function(DF){
+Ploter_Bias<- function(DF){
+  DF_Plot <- melt(DF ,  id.vars = 'train_size', variable.name = 'series')
+  p <- ggplot(DF_Plot, aes(train_size, value)) + geom_line(aes(colour = series))
+  p
+}
+
+Get_Ploter_DF_Bias_V2<- function(DF, vec_train_size){
+  
+  DF_sl <- data.frame(
+    train_size = vec_train_size,
+    sl_rf = DF$sl_rf, 
+    sl_bart = DF$sl_bart, 
+    sl_rf_B = DF$sl_rf_B, 
+    sl_bart_B = DF$sl_bart_B, 
+    sl_rf_K = DF$sl_rf_K,
+    sl_bart_K = DF$sl_bart_K)
+  
+  DF_tl <- data.frame(
+    train_size = vec_train_size,
+    tl_rf = DF$tl_rf,
+    tl_bart = DF$tl_bart,
+    tl_rf_B = DF$tl_rf_B,
+    tl_bart_B = DF$tl_bart_B,
+    tl_rf_K = DF$tl_rf_K,
+    tl_bart_K = DF$tl_bart_K)
+  
+  DF_xl <- data.frame( 
+    train_size = vec_train_size,
+    xl_rf = DF$xl_rf,
+    xl_bart = DF$xl_bart,
+    xl_rf_B = DF$xl_rf_B,
+    xl_bart_B = DF$xl_bart_B,
+    xl_rf_K = DF$xl_rf_K,
+    xl_bart_K = DF$xl_bart_K)
+  
+  list_rtn = list(DF_sl=DF_sl, DF_tl=DF_tl, DF_xl=DF_xl)
+  return(list_rtn)
+  
+}
+
+Get_Ploter_DF_Bias<- function(DF, vec_trials){
+  
+  sl_rf = c()
+  sl_bart = c()
+  tl_rf = c()
+  tl_bart = c()
+  xl_rf = c()
+  xl_bart = c()
+  
+  sl_rf_B = c()
+  sl_bart_B = c()
+  tl_rf_B = c()
+  tl_bart_B = c()
+  xl_rf_B = c()
+  xl_bart_B = c()
+  
+  sl_rf_K = c()
+  sl_bart_K = c()
+  tl_rf_K = c()
+  tl_bart_K = c()
+  xl_rf_K = c()
+  xl_bart_K = c()
+  
+  for(v in vec_trials){
+    sl_rf_v = mean(DF$sl_rf[1:v])
+    sl_rf = append(sl_rf, sl_rf_v)
+    
+    sl_bart_v = mean(DF$sl_bart[1:v])
+    sl_bart = append(sl_bart, sl_bart_v)
+    
+    tl_rf_v = mean(DF$tl_rf[1:v])
+    tl_rf = append(tl_rf, tl_rf_v)
+    
+    tl_bart_v = mean(DF$tl_bart[1:v])
+    tl_bart = append(tl_bart, tl_bart_v)
+    
+    xl_rf_v = mean(DF$xl_rf[1:v])
+    xl_rf = append(xl_rf, xl_rf_v)
+    
+    xl_bart_v = mean(DF$xl_bart[1:v])
+    xl_bart = append(xl_bart, xl_bart_v)
+    
+    
+    sl_rf_B_v = mean(DF$sl_rf_B[1:v])
+    sl_rf_B = append(sl_rf_B, sl_rf_B_v)
+    
+    sl_bart_B_v = mean(DF$sl_bart_B[1:v])
+    sl_bart_B = append(sl_bart_B, sl_bart_B_v)
+    
+    tl_rf_B_v = mean(DF$tl_rf_B[1:v])
+    tl_rf_B = append(tl_rf_B, tl_rf_B_v)
+    
+    tl_bart_B_v = mean(DF$tl_bart_B[1:v])
+    tl_bart_B = append(tl_bart_B, tl_bart_B_v)
+    
+    xl_rf_B_v = mean(DF$xl_rf_B[1:v])
+    xl_rf_B = append(xl_rf_B, xl_rf_B_v)
+    
+    xl_bart_B_v = mean(DF$xl_bart_B[1:v])
+    xl_bart_B = append(xl_bart_B, xl_bart_B_v)
+    
+    
+    sl_rf_K_v = mean(DF$sl_rf_K[1:v])
+    sl_rf_K = append(sl_rf_K, sl_rf_K_v)
+    
+    sl_bart_K_v = mean(DF$sl_bart_K[1:v])
+    sl_bart_K = append(sl_bart_K, sl_bart_K_v)
+    
+    tl_rf_K_v = mean(DF$tl_rf_K[1:v])
+    tl_rf_K = append(tl_rf_K, tl_rf_K_v)
+    
+    tl_bart_K_v = mean(DF$tl_bart_K[1:v])
+    tl_bart_K = append(tl_bart_K, tl_bart_K_v)
+    
+    xl_rf_K_v = mean(DF$xl_rf_K[1:v])
+    xl_rf_K = append(xl_rf_K, xl_rf_K_v)
+    
+    xl_bart_K_v = mean(DF$xl_bart_K[1:v])
+    xl_bart_K = append(xl_bart_K, xl_bart_K_v)
+    
+  }
+  
+  DF_sl <- data.frame(
+    trials = vec_trials,
+    sl_rf = sl_rf, 
+    sl_bart = sl_bart, 
+    sl_rf_B = sl_rf_B, 
+    sl_bart_B = sl_bart_B, 
+    sl_rf_K = sl_rf_K,
+    sl_bart_K = sl_bart_K)
+  
+  DF_tl <- data.frame(
+    trials = vec_trials,
+    tl_rf = tl_rf,
+    tl_bart = tl_bart,
+    tl_rf_B = tl_rf_B,
+    tl_bart_B = tl_bart_B,
+    tl_rf_K = tl_rf_K,
+    tl_bart_K = tl_bart_K)
+   
+  DF_xl <- data.frame( 
+    trials = vec_trials,
+    xl_rf = xl_rf,
+    xl_bart = xl_bart,
+    xl_rf_B = xl_rf_B,
+    xl_bart_B = xl_bart_B,
+    xl_rf_K = xl_rf_K,
+    xl_bart_K = xl_bart_K)
+  
+  list_rtn = list(DF_sl=DF_sl, DF_tl=DF_tl, DF_xl=DF_xl)
+  return(list_rtn)
+
+}
+Ploter_CI<- function(DF){
   
   p <- ggplot(DF, aes(x=Average_CI_Length, y=Coverage, color=ColorGroup, shape=ShapeGroup)) + geom_point() +geom_text_repel(aes(label=PointName), size=5) 
   #+ scale_x_continuous(expand = c(.025, .025)) + scale_y_continuous(expand = c(.2, .2))
